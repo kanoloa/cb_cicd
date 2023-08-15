10521209ee6081fdc431a87d98640f0a685f0151
kanoloa <46836245+kanoloa@users.noreply.github.com>
2023-08-15 10:56:29 +0900

    #2744 Create sample.js

---
+++ sample.js
@@ -0,0 +1,16 @@
+# modify by kanoloa, 23 May 2023
+# do not use this script in production code.
+# addtional lise.
+
+let cb = 2212;
+let factor = 2.14;
+let version = "22.10 sp3";
+
+function sample() {
+  for (let i = 0; i < cb; i++) {
+    logger.debug("counter = " + i + ", powered = " + (i * factor));
+  }
+  logger.debug("this code was executed using " + version);
+}
+
+### end of javascript code.
